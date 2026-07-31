2e6469616c79960755f3a6a0bc656c6b5d69ccba
Surya-58 <mrsuryas445@gmail.com>
2026-07-31 22:47:33 +0530

    Initialized the config

---
+++ Backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async() => {
+    try {
+        console.log("Connecting to mongoDB...");
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export default connectDB
+
