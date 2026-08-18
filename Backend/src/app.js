432c19e912ef7349344ef39fdcce9c834acbe103
Surya-58 <mrsuryas445@gmail.com>
2026-08-18 12:08:16 +0530

    Login verification is completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import productRouter from "./routes/productRoutes.js"
+import userRouter from "./routes/userRoutes.js";
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/api/product", productRouter)
+
+app.use("/api/user",userRouter)
 
 export default app
---
+++ Backend/src/controllers/userController.js
@@ -0,0 +1,85 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.json({
+        success: true,
+        message: "User Registered Successfully",
+        user:{
+            name: user.name,
+            email: user.email
+        }
+    })
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+        success: false,
+        message: error.message
+    })
+  }
+};
+
+export const loginUser = async(req,res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email})
+
+    if(!user){
+      return res.json({
+        success: false,
+        message: "User does not exist"
+      })
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    )
+
+    if(!isPasswordCorrect){
+      return res.json({
+        success: false,
+        message: "Invalid Password"
+      })
+    }
+
+    res.json({
+      success: true,
+      message: "Login successful",
+      user:{
+        name: user.name,
+        email: user.email
+      }
+    })
+    
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      success: false,
+      message: error.message
+    })
+    
+  }
+}
