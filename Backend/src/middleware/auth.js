a97c85b86ca38d95f95eae8d6df1dd2392fb72de
Surya-58 <mrsuryas445@gmail.com>
2026-08-18 12:56:48 +0530

    Authentication middleware completed

---
+++ Backend/src/middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken"
+
+const authMiddkeware = async(req,res,next) => {
+    try {
+        const token = req.headers.authorization
+
+        if(!token){
+            return res.json({
+                success: false,
+                message: "Not authorized. Login again"
+            })
+        }
+
+        const actualToken = token.split(" "[1])
+
+        const decoded = jwt.verify(
+            actualToken,
+            process.env.JWT_SECRET
+        )
+
+        req.userId = decoded.id
+
+        next()
+
+    } catch (error) {
+        console.log(error);
+        
+        res.json({
+            success: false,
+            message: "Invalid Token"
+        })
+    }
+}
+export default authMiddkeware
